ca29f2a8684ae568c28bbf693d0aef4eba048bd9
mehdiMpsoft <Mahdi.khadrahui@mpsoft.com.tn>
2023-02-08 20:47:00 +0100

    rest training

--- src/main/java/com/dev/spring/datajpa/SpringBootDataJpaApplication.java
+++ src/main/java/com/dev/spring/datajpa/SpringBootDataJpaApplication.java
@@ -1,4 +1,4 @@
-package com.bezkoder.spring.datajpa;
+package com.dev.spring.datajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/dev/spring/datajpa/controller/TutorialController.java
+++ src/main/java/com/dev/spring/datajpa/controller/TutorialController.java
@@ -1,4 +1,4 @@
-package com.bezkoder.spring.datajpa.controller;
+package com.dev.spring.datajpa.controller;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bezkoder.spring.datajpa.model.Tutorial;
-import com.bezkoder.spring.datajpa.repository.TutorialRepository;
+import com.dev.spring.datajpa.model.Tutorial;
+import com.dev.spring.datajpa.repository.TutorialRepository;
 
 @CrossOrigin(origins = "http://localhost:8081")
 @RestController
